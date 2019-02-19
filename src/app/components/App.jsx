627baec975d2a512fdda9ea746101dea070f8a36
Sergei Bakaev <bakaev@yandex.ru>
2019-02-19 22:45:26 +0300

    refactoring of project struct

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 
-import Main from './MainContainer';
+import Main from '../containers/MainContainer';
 
 const App = props => (
   <Provider store={props.store}>
--- src/app/containers/MainContainer.js
+++ src/app/containers/MainContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 
-import Main from './Main.jsx';
+import Main from '../components/Main.jsx';
 
 const mapStateToProps = state => ({ value: state });
 
